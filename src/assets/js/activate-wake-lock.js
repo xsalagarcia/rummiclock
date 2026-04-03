e47649d0424ddfa6b3e6366573ca0b99f41f9fe9
xsalagarcia <xsalagarcia@gmail.com>
2026-04-03 16:31:46 +0200

    WakeLock improvement (document not visible, visible again re-activates wake lock)

--- src/assets/js/activate-wake-lock.js
+++ src/assets/js/activate-wake-lock.js
@@ -1,7 +1,28 @@
 /**
  * This script tries to activate Wake Lock, preventing screen lock on mobile devices.
  */
+let wakeLock = null;
+function requestWakeLock(){
+    if (!("wakeLock" in navigator)) return;  // only if navigator suports wakeLock
 
-const wakeLock = navigator.wakeLock.request("screen");
-wakeLock.then(()=>{console.log("Wake Lock active")});
-wakeLock.catch(()=>console.log("Error trying to activate Wake lock"));
+    if (wakeLock === null && document.visibilityState === "visible"){  // only if we don't have wakeLock and document is visible
+        const wakeLockPromise = navigator.wakeLock.request("screen");
+        wakeLockPromise.then((wl)=>{
+            wakeLock = wl;
+            console.log("Wake Lock active");
+            wakeLock.addEventListener("release", ()=>{  // when wakeLock release event occurrs wakeLock will be null, for the next activation.
+                wakeLock = null;
+                console.log("Wake lock released");
+            })
+        });
+        wakeLockPromise.catch(()=>console.log("Error trying to activate Wake lock"));
+
+    }
+}
+
+// activation everytime document visibility change.
+document.addEventListener("visibilitychange", (e)=>{
+    requestWakeLock();
+});
+
+requestWakeLock();  //first time activation.
